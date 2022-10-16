c082791744cae8011cf198fc7cb93d41a945d597
Matthew Mineeth <minithbmatthew@gmail.com>
2022-10-17 00:50:12 +0530

    last change officialy

--- auth.py
+++ auth.py
@@ -20,7 +20,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('pay.html'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
